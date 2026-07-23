6eb2b8a6a0b2d77a223005afd9760aba2ba22aec
lenymrq <lenymrq@gmail.com>
2026-07-24 01:06:08 +0200

    feat(battery): use sysfs helper struct

--- src/blocks/battery.rs
+++ src/blocks/battery.rs
@@ -1,38 +1,13 @@
-use std::error::Error;
-use std::fs::read_to_string;
-use std::path::PathBuf;
 use std::sync::mpsc::Sender;
 use std::thread::sleep;
 use std::time::Duration;
 
+use crate::blocks::utils::SysFs;
 use crate::blocks::BlockUpdate;
 use crate::utils::get_device_dir;
 
 const DEFAULT_INTERVAL: u64 = 5;
 const POWER_SUPPLY_DEVICES_DIR: &str = "/sys/class/power_supply";
-
-struct Battery {
-    device_path: PathBuf,
-}
-
-impl Battery {
-    fn new(device_path: PathBuf) -> Battery {
-        Battery {
-            device_path: device_path,
-        }
-    }
-
-    fn read_capacity(&self) -> Result<u8, Box<dyn Error>> {
-        let capacity = read_to_string(self.device_path.join(PathBuf::from("capacity")))?;
-        return Ok(capacity.trim().parse()?);
-    }
-
-    fn read_status(&self) -> Result<String, Box<dyn Error>> {
-        return Ok(String::from(
-            read_to_string(self.device_path.join(PathBuf::from("status")))?.trim(),
-        ));
-    }
-}
 
 pub fn run(block_id: usize, sender: Sender<BlockUpdate>) {
     let power_supply_path =
@@ -41,14 +16,14 @@ pub fn run(block_id: usize, sender: Sender<BlockUpdate>) {
             _ => todo!("handle error"),
         };
 
-    let battery = Battery::new(power_supply_path);
+    let battery = SysFs::new(power_supply_path);
 
     loop {
-        let capacity = match battery.read_capacity() {
+        let capacity = match battery.read_u8("capacity") {
             Ok(value) => value,
-            Err(e) => todo!("{}", e),
+            Err(_) => todo!("handle error"),
         };
-        let status = match battery.read_status() {
+        let status = match battery.read_string("status") {
             Ok(value) => value,
             Err(_) => todo!("handle error"),
         };
